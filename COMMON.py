03724205ce01654ef72e2f432dcf65e00b4aeb23
hshin-git <hiromasa.shin@gmail.com>
2020-05-05 12:51:28 +0900

    add common parameter

---
+++ COMMON.py
@@ -0,0 +1,27 @@
+## 出力先
+DATA_PATH = "./gfs"
+CONF_PATH = "./conf"
+INFO_PATH = "./info"
+FCST_PATH = "./forecast"
+HCST_PATH = "./hindcast"
+CHRT_PATH = "./chart"
+TILE_PATH = "./tile"
+PLOT_PATH = "./graph"
+
+## グラフ
+MPLSTYLE='default'
+#MPLSTYLE = 'bmh'
+#MPLSTYLE = 'dark_background'
+#MPLSTYLE = 'fivethirtyeight'
+#MPLSTYLE = 'ggplot'
+#MPLSTYLE = 'seaborn-dark'
+
+## 天気図
+AREA = [115,155,20+0,50]	# WEST,EAST,SOUTH,NORTH
+FIGSIZE = (14,12)
+SHRINK = 0.9
+LINEWIDTH = 2
+FONTSIZE = 16
+TRANSPARENT = True
+PAD_INCHES = 1
+
